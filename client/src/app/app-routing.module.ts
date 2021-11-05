beafc8e84fdfff4f8500c726474746291e8c45ee
soviljn <sovilj94@gmail.com>
2021-11-05 14:47:28 +0100

    End of section 14

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -14,6 +14,8 @@ const routes: Routes = [
   {path: 'server-error', component: ServerErrorComponent},
   {path: 'not-found', component: NotFountComponent},
   {path: 'shop', loadChildren: () => import('./shop/shop.module').then(mod => mod.ShopModule)},
+  {path: 'basket', loadChildren: () => import('./basket/basket.module').then(mod => mod.BasketModule)},
+  {path: 'checkout', loadChildren: () => import('./checkout/checkout.module').then(mod => mod.CheckoutModule)},
   {path: '**', redirectTo:'', pathMatch:'full'}
 
 ];
