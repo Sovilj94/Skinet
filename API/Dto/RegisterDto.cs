359dbf9deef757e363342655461277d473d67d70
Sovilj94 <sovilj94@gmail.com>
2021-11-16 23:22:37 +0100

    End of section 16 server side Validation

--- API/Dto/RegisterDto.cs
+++ API/Dto/RegisterDto.cs
@@ -1,9 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace API.Dto
 {
     public class RegisterDto
     {
+        [Required]
         public string DisplayName { get; set; }
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+        [Required]
+        [RegularExpression
+        ("(?=^.{6,10}$)(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&amp;*()_+}{&quot;:;'?/&gt;.&lt;,])(?!.*\\s).*$", 
+        ErrorMessage = "Password is not strong enaugh")]
         public string Password { get; set; }
     }
 }
